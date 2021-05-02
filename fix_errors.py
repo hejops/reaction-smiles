55b3467e2b52231829e8cccfaeac2778e8ea78a8
Joseph <hejops1@gmail.com>
2021-05-02 18:04:37 +0200

    implement cell replace and image redraw

--- fix_errors.py
+++ fix_errors.py
@@ -5,12 +5,15 @@ from pyfzf.pyfzf import FzfPrompt
 import csv
 import logging
 import os
+import pandas as pd
 import shutil
 import sys
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 from smartjoin import *
+from drawops import *
+
 
 def get_smarts(reactants):
     delim = " + "
@@ -18,11 +21,13 @@ def get_smarts(reactants):
     rxns = [delim.join(x["names"]) for x in list(successful_rxns.values())]
 
     fzf = FzfPrompt()
-    chosen_rxn = fzf.prompt(rxns, '--reverse')[0].split(delim)
+    chosen_rxn = fzf.prompt(rxns, "--reverse")[0].split(delim)
 
     for val in successful_rxns.values():
         if chosen_rxn in val.values():
+            # TODO: also return the rxns, so that the csv can be updated
             return val["smarts"]
+
 
 def main():
     """
@@ -32,17 +37,24 @@ def main():
     file = sys.argv[1]
 
     # TODO: int range https://stackoverflow.com/a/6512463
-    line = sys.argv[2]
-    # smarts = sys.argv[3]
+    line = int(sys.argv[2])
 
-    f = open(file)
-    reader = csv.reader(f)
-    row = [x for x in reader if x[0] == line][0]
-    pdt_to_fix = [x.split(">>")[0] for x in row if ">>" in x][0]
+    df = pd.read_csv(file)
+    row = df["Line"] == line
+    # row = df.loc[df["Line"] == line]
+    bad_rxn = df.loc[row, "SMILES"].values.item().split(">>")[0]
+    N = df.loc[row, "N"].values.item()
 
-    smarts = get_smarts(pdt_to_fix)
-    use_known_smarts(pdt_to_fix, smarts)
+    smarts = get_smarts(bad_rxn)
+    fixed_rxn = use_known_smarts(bad_rxn, smarts)
+
+    df.at[row, "SMILES"] = fixed_rxn
+    df.to_csv("test.csv", index=False)
+    imgpath = f"reactions/{line}_{N}.png"
+    draw_mol(fixed_rxn, imgpath)
+
+    # sys.exit()
+
 
 if __name__ == "__main__":
     main()
-
