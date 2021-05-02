cd07d6c3cc1d743cc0d5e014df47cceb02ee6cab
Joseph <hejops1@gmail.com>
2021-05-02 18:24:07 +0200

    implement loop

--- fix_errors.py
+++ fix_errors.py
@@ -29,31 +29,45 @@ def get_smarts(reactants):
             return val["smarts"]
 
 
-def main():
+def main(smarts=None):
     """
     Docstring
     """
 
     file = sys.argv[1]
 
-    # TODO: int range https://stackoverflow.com/a/6512463
-    line = int(sys.argv[2])
+    # TODO: argparse with int range https://stackoverflow.com/a/6512463
+    start = int(sys.argv[2])
+
+    if len(sys.argv) == 4:
+        end = int(sys.argv[3])
+    else:
+        end = start
+
+    lines = range(start, end + 1)
+    # sys.exit()
 
     df = pd.read_csv(file)
-    row = df["Line"] == line
-    # row = df.loc[df["Line"] == line]
-    bad_rxn = df.loc[row, "SMILES"].values.item().split(">>")[0]
-    N = df.loc[row, "N"].values.item()
 
-    smarts = get_smarts(bad_rxn)
-    fixed_rxn = use_known_smarts(bad_rxn, smarts)
+    for line in lines:
 
-    df.at[row, "SMILES"] = fixed_rxn
-    df.to_csv("test.csv", index=False)
-    imgpath = f"reactions/{line}_{N}.png"
-    draw_mol(fixed_rxn, imgpath)
+        row = df["Line"] == line
+        bad_rxn = df.loc[row, "SMILES"].values.item().split(">>")[0]
+        N = df.loc[row, "N"].values.item()
 
-    # sys.exit()
+        if not smarts:
+            smarts = get_smarts(bad_rxn)
+
+        fixed_rxn = use_known_smarts(bad_rxn, smarts)
+        # TODO: exit if wrong smarts used (i.e. overshot)
+
+        df.at[row, "SMILES"] = fixed_rxn
+        # TODO: once this is robust, remember to reuse the filename
+        df.to_csv("test.csv", index=False)
+        imgpath = f"reactions/{line}_{N}.png"
+        draw_mol(fixed_rxn, imgpath)
+
+        print(f"Fixed reaction {line}")
 
 
 if __name__ == "__main__":
