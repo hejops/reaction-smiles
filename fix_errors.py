558d553cc4a3487e50ba8b33e4a72a5ef3fdf786
Joseph <hejops1@gmail.com>
2021-05-20 10:42:07 +0200

    restructure, move for loop into fix_rxns

--- fix_errors.py
+++ fix_errors.py
@@ -31,42 +31,45 @@ def get_smarts(reactants: str, line: str):
             return val["smarts"]
 
 
-def fix_rxn(column: str, line: str, x: list):
+def fix_rxns(column: str, lines: list[int], x: list):
 
-    # row = df["Line"] == line
-    row = df[column] == line
+    for line in lines:
+        # row = df["Line"] == line
+        row = df[column] == line
 
-    # if column != "Line":
-    line = df.loc[row, "Line"].values.item()
-    N = df.loc[row, "N"].values.item()
-    # row.loc["N"]?
+        # if column != "Line":
+        line = df.loc[row, "Line"].values.item()
+        N = df.loc[row, "N"].values.item()
+        # row.loc["N"]?
 
-    bad_rxn = df.loc[row, "SMILES"].values.item()
-    draw_mol(bad_rxn, show=True)
-    print(bad_rxn)
+        bad_rxn = df.loc[row, "SMILES"].values.item()
+        draw_mol(bad_rxn, show=True)
+        print(bad_rxn)
 
-    bad_rxn = bad_rxn.split(">>")[0]
+        bad_rxn = bad_rxn.split(">>")[0]
 
-    # if not smarts:
-    smarts = get_smarts(bad_rxn, line)
-    # TODO: currently, looping is not much use if same smarts is always used
-    # use a try/except?
+        # if not smarts:
+        smarts = get_smarts(bad_rxn, line)
+        # TODO: currently, looping is not much use if same smarts is always used
+        # use a try/except?
 
-    fixed_rxn = use_known_smarts(bad_rxn, smarts)
+        fixed_rxn = use_known_smarts(bad_rxn, smarts)
 
-    if "c" in fixed_rxn:
-        force_fixed_rxn = input_with_prefill(
-            "Unusual aromatic? Fix required: ", fixed_rxn
-        )
-        fixed_rxn = force_fixed_rxn
+        # added because of strange reactions like this, where H on N is lost for some reason
+        # C[Si](C)(C)OC1=CCCC1.CC1=C([CH+]C2=CC=C(C)C=C2)C2=C(N1)C=CC=C2>>Cc1ccc(C(c2c(C)[nH]c3ccccc23)C2CCC[C+]2O[Si](C)(C)C)cc1
+        if "c" in fixed_rxn:
+            force_fixed_rxn = input_with_prefill(
+                "Unusual aromatic? Fix required: ", fixed_rxn
+            )
+            fixed_rxn = force_fixed_rxn
 
-    df.at[row, "SMILES"] = fixed_rxn
-    imgpath = f"reactions/{line}_{N}.png"
-    draw_mol(fixed_rxn, imgpath)
+        df.at[row, "SMILES"] = fixed_rxn
+        imgpath = f"reactions/{line}_{N}.png"
+        draw_mol(fixed_rxn, imgpath)
 
-    print(f"Fixed reaction {line}")
-    # sys.exit()
-    x.append(line)
+        print(f"Fixed reaction {line}")
+        # sys.exit()
+        x.append(line)
 
 
 def input_with_prefill(prompt: str, text: str) -> str:
@@ -116,32 +119,24 @@ def main():
 
     args = parser.parse_args()
 
-    # TODO: argparse with int range https://stackoverflow.com/a/6512463
-    # start = int(sys.argv[2])
-    # if len(sys.argv) == 4:
-    #     end = int(sys.argv[3])
-    # else:
-    #     end = start
-    # lines = range(start, end + 1)
-    # # sys.exit()
-
-    # for line in lines:
-
     global df
     df = pd.read_csv(args.file)
     print("Loaded", args.file)
     x = []
 
-    while True:
+    if args.input_file:
+        with open(args.input_file) as f:
+            lines = [int(line.rstrip()) for line in f if not line.startswith("#")]
+            fix_rxns(args.column, lines, x)
 
-        try:
+        df.to_csv(args.file, index=False)
+        print(f"All lines in {args.file} processed")
 
-            if args.input_file:
-                with open(args.input_file) as f:
-                    lines = [line.rstrip() for line in f if not line.startswith("#")]
-                # print(lines)
+    else:
 
-            else:
+        while True:
+
+            try:
 
                 lines = input("Reaction number: ")
 
@@ -160,18 +155,15 @@ def main():
                 else:
                     lines = [lines]
 
-            lines = [int(line) for line in lines]
+                lines = [int(line) for line in lines]
 
-            for line in lines:
-                fix_rxn(args.column, line, x)
+                fix_rxns(args.column, lines, x)
 
-        # except KeyboardInterrupt:
-        except:
-            df.to_csv(args.file, index=False)
-            print(f"Wrote: {args.file}")
-            sys.exit()
-
-    # df.to_csv(file, index=False)
+            # except KeyboardInterrupt:
+            except:
+                df.to_csv(args.file, index=False)
+                print(f"Wrote: {args.file}")
+                sys.exit()
 
 
 if __name__ == "__main__":
